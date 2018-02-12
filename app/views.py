22b95fd360187834f364fa207da2c508578cd08a
Kevaughn Batchelor <justbatchelor@outlook.com>
2018-02-12 15:25:31 +0000

    Complete starter lab2

--- app/views.py
+++ app/views.py
@@ -4,10 +4,9 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -24,6 +23,15 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+##function return the formatted date using the built in date function imported from the python library.
+def format_date_joined():
+    date_joined = datetime.date(2018, 2, 19)
+    return "Joined " + date_joined.strftime("%B, %Y")
+
+@app.route('/profile/')
+def profile():
+    date = format_date_joined()
+    return render_template('profile.html', name="Kevuaghn Batchelor", date=date) 
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -35,7 +43,6 @@ def send_text_file(file_name):
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
 
-
 @app.after_request
 def add_header(response):
     """
@@ -46,7 +53,6 @@ def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
-
 
 @app.errorhandler(404)
 def page_not_found(error):
